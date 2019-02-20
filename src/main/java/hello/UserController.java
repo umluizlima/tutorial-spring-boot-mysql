1cb966e4461fdc96661ebe20809a9ebaffbb9268
umluizlima <umluizlima@gmail.com>
2019-02-20 09:57:25 -0300

    Add user controller and repository

---
+++ src/main/java/hello/UserController.java
@@ -0,0 +1,37 @@
+package hello;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/demo")
+public class UserController {
+
+    // This means to get the bean called userRepository, which is auto-generated
+    // by Spring, to handle the data
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/add")
+    public @ResponseBody String addNewUser (
+            @RequestParam String name,
+            @RequestParam String email) {
+        // @ResponseBody means the returned String is the response, not a view name
+        // @RequestParam means it is a parameter from the GET or POST request
+        User n = new User();
+        n.setName(name);
+        n.setEmail(email);
+        userRepository.save(n);
+        return "Saved";
+    }
+
+    @GetMapping("/all")
+    public @ResponseBody Iterable<User> getAllUsers() {
+        // This returns a JSON or XML with the users
+        return userRepository.findAll();
+    }
+}
